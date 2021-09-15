0409ccb3b8749074a7ecd779d52e6e6dbf2f7e88
Kel Cecil <kelcecil@praisechaos.com>
2021-09-15 08:53:41 -0400

    Add new collection controller

---
+++ lib/video_game_collection_web/controllers/collection_controller.ex
@@ -0,0 +1,21 @@
+defmodule VideoGameCollectionWeb.CollectionController do
+  use VideoGameCollectionWeb, :controller
+  alias VideoGameCollection.{Collections, Users}
+  alias VideoGameCollection.User
+
+
+  def index(conn, %{user_id: user_id} = _params) do
+    with %User{} = user <- Users.get(user_id) do
+      collection = Collections.list(user)
+
+      conn
+      |> put_status(200)
+      |> render("index.html", collection: collection, user: user)
+    else
+      nil ->
+        conn
+        |> put_status(404)
+        |> render(:"404")
+      end
+  end
+end
