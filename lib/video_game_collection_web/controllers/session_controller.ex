1935e61a28340e6c0ef95874df7e9c0b79df3e8f
Kel Cecil <kelcecil@praisechaos.com>
2021-10-22 08:26:00 -0400

    Add login controller functions for create

--- lib/video_game_collection_web/controllers/session_controller.ex
+++ lib/video_game_collection_web/controllers/session_controller.ex
@@ -1,11 +1,24 @@
 defmodule VideoGameCollectionWeb.SessionController do
   use VideoGameCollectionWeb, :controller
+  alias VideoGameCollection.Accounts.UserAuth
+  alias VideoGameCollection.User
 
   def new(conn, _param) do
-    render(conn, "new.html")
+    render(conn, "new.html", error_message: nil)
   end
 
-  def create(conn, _param) do
-
+  def create(conn, %{"session" => %{"email" => email, "password" => password}}) do
+    with %User{id: id} <- UserAuth.login(email, password),
+     token <- Phoenix.Token.sign(VideoGameCollectionWeb.Endpoint, "randonized_salt", id) do
+      conn
+      |> clear_session()
+      |> configure_session(renew: true)
+      |> put_session(:user_token, token)
+      |> redirect(to: Routes.collection_path(conn, :index))
+    else
+      nil ->
+        conn
+        |> render("new.html", error_message: "Invalid username or password")
+    end
   end
 end
