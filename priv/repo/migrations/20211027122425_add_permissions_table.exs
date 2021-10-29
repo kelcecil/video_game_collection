eaedc7aeaa3e1b22848b58ea280c3be9e69d6a65
Kel Cecil <kelcecil@praisechaos.com>
2021-10-29 07:53:18 -0400

    Remove primary key and timestamps from join table
    Ecto won't write these fields unless I'm using a schema
    for my join table. I'm currently not doing that to keep
    the example simple.

--- priv/repo/migrations/20211027122425_add_permissions_table.exs
+++ priv/repo/migrations/20211027122425_add_permissions_table.exs
@@ -3,11 +3,9 @@ defmodule VideoGameCollection.Repo.Migrations.AddPermissionsTable do
   alias VideoGameCollection.User
 
   def change do
-    create table("permissions") do
+    create table("permissions", primary_key: false) do
       add :owner_id, references(:users)
       add :receipient_id, references(:users)
-
-      timestamps()
     end
 
     create index(:permissions, [:owner_id, :receipient_id])
