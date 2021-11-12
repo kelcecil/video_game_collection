66522df3c3b646179c34533c68167a43cb3d6d73
Kel Cecil <kelcecil@praisechaos.com>
2021-11-12 07:48:21 -0500

    Add example liveview chat function

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,12 @@ import "../css/app.css"
 // Import deps with the dep name or local files with a relative path, for example:
 //
 import {Socket} from "phoenix"
-import socket from "./socket"
+//import socket from "./socket"
 //
 import "phoenix_html"
+
+import {LiveSocket} from "phoenix_live_view"
+
+let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+liveSocket.connect()
---
+++ lib/video_game_collection_web/live/chat/chat_live.ex
@@ -0,0 +1,33 @@
+defmodule VideoGameCollectionWeb.ChatLive do
+  alias VideoGameCollection.Accounts
+  use VideoGameCollectionWeb, :live_view
+  alias Phoenix.PubSub
+
+  @chat_topic "chat:all"
+
+  def mount(_params, %{"user_token" => user_token}, socket) do
+    PubSub.subscribe(VideoGameCollection.PubSub, @chat_topic)
+
+    # Verify token and look up the user.
+    {:ok, id} = Phoenix.Token.verify(VideoGameCollectionWeb.Endpoint, "randomized_salt", user_token)
+    {:ok,
+      socket
+      |> assign(:authenticated_user, Accounts.get(id))
+      |> assign(:messages, [])
+    }
+  end
+
+  def handle_event("new-message", payload, socket) do
+    name = socket.assigns[:authenticated_user].name
+    message = payload |> Map.get("chat") |> Map.get("message")
+
+    broadcast_payload = {:new_message, name, DateTime.now!("Etc/UTC"), message}
+    PubSub.broadcast(VideoGameCollection.PubSub, @chat_topic, broadcast_payload)
+    {:noreply, socket}
+  end
+
+  def handle_info({:new_message, name, timestamp, message}, socket) do
+    updated_messages = [{name, timestamp, message} | socket.assigns.messages]
+    {:noreply, assign(socket, :messages, updated_messages)}
+  end
+end
