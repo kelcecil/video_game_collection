cf43ae4dea41138cbd1cc9e9a709b1f453395227
Kel Cecil <kelcecil@praisechaos.com>
2021-11-19 08:01:52 -0500

    Cleanup game card
    Add the edit link to the card and make a better box shadow
    for the component using alpha effects.

--- lib/video_game_collection_web/views/collection_view.ex
+++ lib/video_game_collection_web/views/collection_view.ex
@@ -5,14 +5,19 @@ defmodule VideoGameCollectionWeb.CollectionView do
     ~H"""
     <div class="game-component">
       <div class="image-section">
-        <img src={@cover_art_url} />
+        <img class="cover-art" src={@cover_art_url} />
       </div>
       <div class="info-section">
         <h3 class="game-title"><%= @name %></h3>
         <p class="console-name">Super Nintendo</p>
         <p><%= @publisher %></p>
       </div>
+      <a href={@edit_link}>Edit</a>
     </div>
     """
+  end
+
+  defp with_edit_link(conn_or_socket, game) do
+    Map.put_new(game, :edit_link, Routes.collection_path(conn_or_socket, :edit, game.id))
   end
 end
